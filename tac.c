37461ef19670aff74e61536797d3b158c9d57da7
Jonathan Sid-Otmane <Watabanga@gmail.com>
2017-01-19 00:19:22 +0100

    Resolves #1

--- tac.c
+++ tac.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(){
 	char s[256];
-	int i;
+	int i, backspace;
+
+	//Reading the file will get messy without it
+	backspace =  isatty(STDOUT_FILENO);
 
 	while(fgets(s,256,stdin)){
 
-		printf("\033[A"); //GOTO previous line
-		printf("\33[2K"); //Erase current line
+		if(backspace){
+			printf("\033[A"); //GOTO previous line
+			printf("\33[2K"); //Erase current line
+		}
 
 		for(i=strlen(s) - 2; i>=0; i--)
 			printf("%c", s[i]);
